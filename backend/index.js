1b9ce31cc93c15d1e79fb007921ccc0cb7e47e1a
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2023-07-01 11:30:38 +0530

    backend add

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import createError from "http-errors";
+import morgen from "morgan";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/auth.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+
+app.get("/", async (req, resq, next) => {
+  resq.send("Hello From Express");
+});
+
+app.use("/auth", authRoute);
+
+app.use(async (req, resq, next) => {
+  next(createError.NotFound());
+});
+
+app.use((err, req, resq, next) => {
+  resq.status(err.status || 500);
+  resq.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is Runing on Port ${PORT}`);
+});
