3d5d7c59d3404d03983e4b469f95a6766e4a7e57
Anubhav-Raj <rajputanubhav65@gmail.com>
2024-02-18 18:16:53 +0530

    fix issue

--- src/pages/createProject/createTask.jsx
+++ src/pages/createProject/createTask.jsx
@@ -87,9 +87,10 @@ const CreateTask = () => {
     initTE({ Select }, "#select2");
     initTE({ Select }, "#select3");
     initTE({ Datepicker, Input });
-  }, []);
+  }, [selectedOptions, targetURL]);
 
   const handleOptionSelect = (event) => {
+    event.preventDefault();
     const selectedValues = Array.from(
       event.target.selectedOptions,
       (option) => option.value
@@ -104,7 +105,7 @@ const CreateTask = () => {
     );
     setTestingType(selectedValues);
   };
-  console.log(testingType);
+  //console.log(testingType);
 
   const token = userData?.token;
 
@@ -818,22 +819,22 @@ const CreateTask = () => {
                           </div>
                           {/* datepicker  */}
                           <div
-                              class="relative w-[240px]  h-[35px]  "
-                              data-te-datepicker-init
-                              data-te-input-wrapper-init
+                            class="relative w-[240px]  h-[35px]  "
+                            data-te-datepicker-init
+                            data-te-input-wrapper-init
+                          >
+                            <input
+                              type="text"
+                              class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                              placeholder="Select a date"
+                            />
+                            <label
+                              for="floatingInput"
+                              class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
                             >
-                              <input
-                                type="text"
-                                class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                placeholder="Select a date"
-                              />
-                              <label
-                                for="floatingInput"
-                                class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                              >
-                                Select a date
-                              </label>
-                            </div>
+                              Select a date
+                            </label>
+                          </div>
                           <div class="col-span-full">
                             <label
                               for="about"
